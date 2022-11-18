d95f75ad8201fd7ab448204367b14d6a29cb10cd
shadowgun302 <115239387+shadowgun302@users.noreply.github.com>
2022-11-18 16:16:45 +0100

    Update Zad1.java

--- src/Zad1.java
+++ src/Zad1.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Zad1 {
+public class Zad1 {
+    public static void main(String[] args) {
+        int[] array = {10,9,8,7,6,5,4,3,2,1};
+
+        int counter = 0;
+        System.out.println("Before: ");
+        System.out.println(Arrays.toString(array));
+
+        for(int j = 0; j < array.length - 1; j++) {
+            if(array[j] > array[j + 1]){
+                int greater = array[j];
+                array[j] = array[j+1];
+                array[j+1] = greater;
+            }
+        }
+        System.out.println("After: ");
+        System.out.println(Arrays.toString(array));
+
+
+    }
 }
